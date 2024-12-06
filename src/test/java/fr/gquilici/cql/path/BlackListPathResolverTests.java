74bbd1238701293e3b2c7e43cf98d8b344fb98a4
gquilici-fb <guilhem.quilici@fermesbio.fr>
2024-12-06 16:05:30 +0100

    Add tests for path resolvers implementations

---
+++ src/test/java/fr/gquilici/cql/path/BlackListPathResolverTests.java
@@ -0,0 +1,133 @@
+package fr.gquilici.cql.path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import fr.gquilici.cql.poc.Company;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Root;
+
+@SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)
+public class BlackListPathResolverTests {
+
+	@Autowired
+	private EntityManager entityManager;
+
+	private CriteriaBuilder builder;
+
+	@BeforeAll
+	void setup() {
+		builder = entityManager.getCriteriaBuilder();
+	}
+
+	@Test
+	void noRegisteredRestriction() {
+		BlackListPathResolver pathResolver = new BlackListPathResolver();
+
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		List<String> patterns = pathResolver.getPatterns(root);
+		assertThat(patterns).isNotNull();
+		assertThat(patterns).isEmpty();
+	}
+
+	@Test
+	void retrieveRegisteredRestrictions() {
+		BlackListPathResolver pathResolver = new BlackListPathResolver();
+		pathResolver.register(Company.class, "code", "employees.**");
+
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		List<String> patterns = pathResolver.getPatterns(root);
+		assertThat(patterns).hasSize(2);
+		assertThat(patterns).contains("code", "employees.**");
+	}
+
+	@Test
+	void restrictLocalProperty() {
+		BlackListPathResolver pathResolver = new BlackListPathResolver();
+		pathResolver.register(Company.class, "code");
+
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		boolean authorized = pathResolver.isAuthorized(root, "code");
+		assertFalse(authorized, "Property <code> should be unreachable");
+
+		authorized = pathResolver.isAuthorized(root, "name");
+		assertTrue(authorized, "Property <name> should be reachable");
+	}
+
+	@Test
+	void restrictWithOneWildcard() {
+		BlackListPathResolver pathResolver = new BlackListPathResolver();
+		pathResolver.register(Company.class, "employees.*");
+
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		boolean authorized = pathResolver.isAuthorized(root, "employees.lastName");
+		assertFalse(authorized, "Property <employees.lastName> should be unreachable");
+
+		authorized = pathResolver.isAuthorized(root, "employees.company.code");
+		assertTrue(authorized, "Property <employees.company.code> should be reachable");
+	}
+
+	@Test
+	void restrictWithTwoWildcards() {
+		BlackListPathResolver pathResolver = new BlackListPathResolver();
+		pathResolver.register(Company.class, "employees.**");
+
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		boolean authorized = pathResolver.isAuthorized(root, "employees.lastName");
+		assertFalse(authorized, "Property <employees.lastName> should be unreachable");
+
+		authorized = pathResolver.isAuthorized(root, "employees.company.code");
+		assertFalse(authorized, "Property <employees.company.code> should be unreachable");
+	}
+
+	@Test
+	void resolveReachableProperty() {
+		BlackListPathResolver pathResolver = new BlackListPathResolver();
+		pathResolver.register(Company.class, "employees.**");
+
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		Path<?> property = pathResolver.resolve(root, "code");
+		assertThat(property.getJavaType()).isEqualTo(String.class);
+	}
+
+	@Test
+	void resolveUnreachableProperty() {
+		BlackListPathResolver pathResolver = new BlackListPathResolver();
+		pathResolver.register(Company.class, "employees.**");
+
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		Exception e = assertThrows(IllegalArgumentException.class,
+				() -> pathResolver.resolve(root, "employees.lastName"), "An unreachable property should raise an exception");
+		assertThat(e.getMessage()).isEqualTo("Le chemin de propriété <employees.lastName> n'est pas supporté");
+	}
+
+}
---
+++ src/test/java/fr/gquilici/cql/path/DefaultPathResolverTests.java
@@ -0,0 +1,95 @@
+package fr.gquilici.cql.path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import fr.gquilici.cql.operator.PathResolver;
+import fr.gquilici.cql.poc.Company;
+import fr.gquilici.cql.poc.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.criteria.Root;
+
+@SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)
+public class DefaultPathResolverTests {
+
+	@Autowired
+	private EntityManager entityManager;
+
+	private CriteriaBuilder builder;
+
+	private PathResolver pathResolver;
+
+	@BeforeAll
+	void setup() {
+		builder = entityManager.getCriteriaBuilder();
+		pathResolver = new DefaultPathResolver();
+	}
+
+	@Test
+	void resolveLocalProperty() {
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		Path<?> property = pathResolver.resolve(root, "name");
+		assertThat(property.getJavaType()).isEqualTo(String.class);
+	}
+
+	@Test
+	void resolveSingularJoinProperty() {
+		CriteriaQuery<Employee> query = builder.createQuery(Employee.class);
+		Root<Employee> root = query.from(Employee.class);
+
+		Path<?> property = pathResolver.resolve(root, "company");
+		assertThat(property.getJavaType()).isEqualTo(Company.class);
+	}
+
+	@Test
+	void resolveCollectionJoinProperty() {
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		Path<?> property = pathResolver.resolve(root, "employees");
+		assertThat(property.getJavaType()).isEqualTo(Employee.class);
+	}
+
+	@Test
+	void resolveNonLocalProperty() {
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		Path<?> property = pathResolver.resolve(root, "employees.lastName");
+		assertThat(property.getJavaType()).isEqualTo(String.class);
+	}
+
+	@Test
+	void resolveNullProperty() {
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		Exception e = assertThrows(IllegalArgumentException.class, () -> pathResolver.resolve(root, null),
+				"A null property should raise an exception");
+		assertThat(e.getMessage()).isEqualTo("Le chemin de la propriété cible ne doit pas être nul");
+	}
+
+	@Test
+	void resolveUnknownProperty() {
+		CriteriaQuery<Company> query = builder.createQuery(Company.class);
+		Root<Company> root = query.from(Company.class);
+
+		Exception e = assertThrows(IllegalArgumentException.class, () -> pathResolver.resolve(root, "foobar"),
+				"An unknown property should raise an exception");
+		assertThat(e.getMessage()).isEqualTo("Le chemin de propriété <foobar> n'est pas supporté");
+	}
+
+}
